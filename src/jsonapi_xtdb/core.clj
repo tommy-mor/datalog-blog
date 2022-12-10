6ded41478637c71c9a86e761df9be1b00a62bc8d
tommy <thmorriss@gmail.com>
2022-12-10 16:42:32 -0500

    finishing touches

--- src/jsonapi_xtdb/core.clj
+++ src/jsonapi_xtdb/core.clj
@@ -13,13 +13,13 @@
 
 
 
-;; Hello all, my name is tommy and I love clojure. You can find me on [github](https://github.com/tommy-mor) or [email me](mailto:thmorriss@gmail.com). This article was created with [clerk](https://github.com/nextjournal/clerk).
+;; Hello all, my name is tommy and I love clojure. You can find me on [github](https://github.com/tommy-mor) or [email me](mailto:thmorriss@gmail.com). This article was created with [clerk](https://github.com/nextjournal/clerk) and published on [clerk garden](https://clerk.garden).
 ;; 
 ;; I am working on a gameserver plugin for my favorite fighting game [MGE](https://wiki.teamfortress.com/wiki/MGE_Mod), and want to be able able to run tournaments automatically on a weekly basis, similar to how many melee groups run low-stakes weekly tournaments. 
 
 ;; Thankfully, the company [challonge](https://challonge.com) (owned now by logitech) hosts software that lets you create and run your own tournaments. It displays the bracket, handles tournament state, player signups, multistage tournaments (swiss/round-robin/group-stage), winners/losers brackets, and much more. It also lets you embed the (realtime updating) bracket as an iframe.
 
-;; It also has an API. So lets get started. Below is an example tournament that I've created. Its to decide which color is the best color. A couple matches have already been played (apparently crimson is favorable over rust by 6 points...)
+;; It also has an API. So lets get started. Below is an example tournament that I've created, to decide which color is the best color. A couple matches have already been played (apparently crimson is favorable over rust by 15 points...)
 
 ^{::clerk/visibility {:code :hide :result :show}}
 (ImageIO/read (io/file "bracket.png"))
@@ -33,15 +33,21 @@
 ^{::clerk/opts {:auto-expand-results? true}}
 (def matches (clojure.edn/read-string (slurp "matches.edn")))
 ;; I asked for matches, I got very complicated piece of data.
-;; The `:data` key has an array of 10 resources, each with a type, id, some attributes, and some relationships.
+;; The `:data` key has a vector of 10 resources, each with a type, id, some attributes, and some relationships.
 
 ;; For a each match in the tournament, its relationships are the two opponents facing off in that match. However, the participant nested in the match `(get-in matches [:data 0 :relationships :player1])` has very little information. If you want to see any `attributes` of a player, you must look in the toplevel `:included` vector and find the right one.
 
-;; The `:included` vector contains all 12 participants (colors) in this tournament.
+;; The `:included` vector contains all 12 participant resources (colors) in this tournament.
 
-;; Also notice that each participant (in the `:included` vector) has a `:misc` attribute. This is any small piece of data you want, and in my case it is the gameserver ID of the player. This is domain specific to my code.
+;; All resources have a uniform shape.
+(distinct (concat (map keys (:data matches))
+                  (map keys (:included matches))))
 
-;; This is a fairly complex nested structure, but it's well thought out, and handles many edge cases. I personally would prefer to expose my data using something like [pathom3](https://github.com/wilkerlucio/pathom3), but I prefer the standardized json-api approach over having arbitrary shapes that are different for every company.
+;; In each participant, there is a `:misc` key in the attributes map. This is a bit of data specific to my domain that challonge allows me to attach to the resource. In my case, it the id of each player in my gameserver.
+
+^{::clerk/opts {:auto-expand-results? true}}
+(-> matches :included first)
+;; At first glance this feels structure feels heavy, but it's well thought out, and handles many edge cases. I personally would prefer to expose my data using something like [pathom3](https://github.com/wilkerlucio/pathom3), but I prefer the standardized json-api approach over having arbitrary shapes that are different for every company.
 
 
 
@@ -72,6 +78,7 @@
          (map (juxt :player1 :player2))
          flatten)))
 
+^{::clerk/opts {:auto-expand-results? true}}
 (first (matches->participating-user-ids matches))
 ;; Nice! But we're still not done. We have the player ids, but its not the entire resource. The piece of data we want is the included resources. Lets try again, but build a join from challonge id to gameserver uuid first... 
 (defn matches->participating-users [matches]
@@ -107,20 +114,16 @@
 
 ;; Enter [xtdb](https://xtdb.com/), the graph database from JUXT. We are receiving a normalized graph of resources from the json-api endpoint, and are computing queries on it. Why not have xtdb run the queries for us.
 
-;; Lets start an xtdb node with an empty configuration map: Instead of persisting the data in one of their [pluggable backends](https://docs.xtdb.com/administration/configuring/#_storage), we store it in plain java datstructures.
+;; Lets start an xtdb node with an empty configuration map: Instead of persisting the data in one of their [pluggable backends](https://docs.xtdb.com/administration/configuring/#_storage), we store it in plain java datstructures by giving an empty map as configuration.
 
-^{::clerk/visibility {:code :show :result :hide}}
+^{::clerk/visibility {:code :show :result :hide}
+  :nextjournal.clerk/no-cache true}
 (def node (xt/start-node {}))
 
 
 ;; Xtdb is schemaless so we don't have to worry about defining any attributes ahead of time.
 
 ;; Although you can keep the documents nested and query them in datalog (you can run `(get-in _ [:attributes :timestamps :startedAt])` directly in the query if you wanted), its cleaner to flatten the data before ingesting it.
-
-;; Because challonge happens to use json-api, each response is predictibly structured such that:
-;;   * every toplevel response has `:data` and `:included` keys. they contain either a single resource, or a list of them.
-;;   * each resource has an `id` key.
-
 
 ;; Lets use this (12 year old!!) function to recursively flatten keys, joining nested keys with a "."
 
@@ -139,15 +142,21 @@
 (flatten-keys (->> matches :data first))
 
 
-;; Much better.
+;; Much better. We don't need to, but we could do the reverse of this operation because [section 7.8.2](https://jsonapi.org/format/#document-member-names-allowed-characters) of the json-api spec forbids "." in key names.
 
 ;; Now lets deal with toplevel responses. 
 
-;; This funciton takes every resource from the response, flattens it, and puts it into our database node.
+;; Because of json-api's uniformity, we can have just one function that handles every possible api response.
+;; In particular, we use these properties:
+;;   * Every toplevel response has `:data` and `:included` keys. They contain either a single resource, or a list of them.
+;;   * Each resource has an `:id` key.
+
+
+;; This function takes every resource from the response, flattens it, and puts it into our database node.
 
 ;; Notice how we tranform the challonge notion `:id` to `:xt/id`, which every xtdb document needs.
 
-(defn ingest-jsonapi-response [resp]
+(defn ingest-jsonapi-response [node resp]
   (let [payload (->> resp
                      ((juxt :data :included))
                      (map (fn [res] (cond
@@ -159,25 +168,37 @@
     (xt/await-tx node (xt/submit-tx node (for [doc payload]
                                            [::xt/put doc])))))
 
-(ingest-jsonapi-response matches)
+(ingest-jsonapi-response node matches)
+
+
 
 ^{::clerk/visibility {:code :hide :result :show}}
 (clerk/md "# Datalog is beautiful")
-;; Now lets ask all the same questions as before, but this time in a declarative style.
+;; Now lets ask all the same questions as before, this time in a declarative style.
 
 (defn xtdb-matches->pending [node]
   "see which matches are pending (need to and can be played)"
-  (map first (xt/q (xt/db node) '{:find [match]
-                                  :where [[match :type "match"]
-                                          [match :attributes.state "open"]]})))
+  (map first (xt/q (xt/db node) '{:find [matchid]
+                                  :where [[matchid :type "match"]
+                                          [matchid :attributes.state "open"]]})))
 
-;; If you have never seen datalog before, dont fret. This query can be read as
-;; "find the id of every document that has the key `:type` with a value of `"match"`, and the
-;; key `:attributes.state` with a value of `"open"`. Each triple in the `:where` clause of the query is defining a rule/restriction in the shape `[entity attribute value]` that the returned documents must comply to.
+^{::clerk/visibility {:code :hide :result :show}}
+(clerk/md "
+ If you have never seen datalog before, don't fret. This query can be read as
+ > find the id of every document that has the key `:type` with a value of `\"match\"`, and the
+ key `:attributes.state` with a value of `\"open\"`.
+
+
+Each triple in the `:where` clause of the query is defining a rule/restriction in the shape `[entity attribute value]` that the returned documents must comply to.
+
+")
 
 ;; The effect of declaring that an attribute must match a data literal like `[match :attribute.state "open"]` is a filtering of documents. This line is like my above function `matches->pending` which took my 10 matches down to only 4 open ones.
-;; This is a nice delcarative filtering, but nothing mind shattering. The magic comes when you put a variable in the value position of the triple. Now any time you use this variable again (in entity or value position of another rule), the values must match up.  
+;; This is a nice delcarative filtering, but nothing mind shattering.
 
+;;The magic comes when you put a variable in the value position of the triple. Now any time you use this variable again (in entity or value position of another rule), the values must match up.  
+
+^{::clerk/opts {:auto-expand-results? true}}
 (xtdb-matches->pending node)
 
 (defn xtdb-matches->participating-users [node]
@@ -196,6 +217,17 @@
 
 ^{::clerk/opts {:auto-expand-results? true}}
 (xtdb-matches->participating-users node)
+
+^{::clerk/opts {:auto-expand-results? true}}
+(xt/q (xt/db node) '{:find [match p1uuid p1name p2uuid p2name]
+                       :where [[match :type "match"]
+                               [match :attributes.state "open"]
+                               [match :relationships.player1.data.id p1]
+                               [match :relationships.player2.data.id p2]
+                               [p1 :attributes.misc p1uuid]
+                               [p1 :attributes.name p1name]
+                               [p2 :attributes.misc p2uuid]
+                               [p2 :attributes.name p2name]]})
 
 
 ;; Now the reverse operation, (find match ids from gameserver uuid), is trivial.
@@ -235,7 +267,7 @@
 (xtdb-names->match node "red" "scarlet")
 
 ;; Datalog can also run arbitraty code inside queries. Here, the challonge api gives us the winner of a match as an integer, even though the primary keys are all strings. 
-;; Easily rectified by calling str on the key before using it furthur.
+;; Easily rectified by calling str on the key before using it further.
 
 (xt/q (xt/db node) '{:find [winnername]
                      :where [[match :type "match"]
@@ -259,6 +291,6 @@
 ^{::clerk/visibility {:code :hide :result :show}}
 (clerk/md "# Conclusion")
 
-;; Thanks to json-api, every response to further api calls moving the tournament forward tan be run through `ingest-jsonapi-resonpse` and be otherwise forgotten.
+;; Thanks to json-api, every response to further api calls moving the tournament forward can be run through `ingest-jsonapi-resonpse` and be otherwise forgotten.
 
 ;; Before xtdb, I would have to write code to answer my questions. After xtdb, every question I can ask of my data is already answered, I just have to phrase the quesiton!
